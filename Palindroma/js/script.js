49ff0d092044d595d6052595832918f1500016dd
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-02-06 18:20:48 +0100

    Fine esercizio Palindroma

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -7,4 +7,22 @@ ATTENZIONE :
 No .reverse, .split e .join
 */
 
-const userWord = prompt("Inserisci una parola")
+// chiedere all'utente una parola
+let parola = prompt("Inserisci una parola")
+
+// creo una funzione
+//palindorma vuol dire che contiene la stessa sequenza di caratteri.
+function palindroma(parola){
+    // Se un carattere non corrisponde al carattere nell'altra metà: NON palindroma
+    for(i=0; i < parola.length / 2; i++){
+        if(parola[i] !== parola[parola.length -i -1]){
+            return false;
+        }
+    }
+    // Parola è palindroma
+    return true;
+}
+
+let risultato = palindroma(parola);
+
+document.getElementById("risultato").innerHTML = `${risultato}`;
